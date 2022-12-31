3dc7003c5c4d4e03f4a6a6a6fd373f425c380174
Roming22 <roming22@gmail.com>
2022-12-31 04:08:05 -0500

    Generate corpus data for code

--- src/myapp/corpus_processor.py
+++ src/myapp/corpus_processor.py
@@ -1,5 +1,8 @@
 """Generate input data from the corpus"""
 import glob
+import os
+
+import git
 
 
 def generate_data(corpus_preferences: dict) -> dict:
@@ -11,7 +14,6 @@ def generate_data(corpus_preferences: dict) -> dict:
     language_data = {}
     language_data.update(generate_i18_data(corpus_preferences["i18n"]))
     language_data.update(generate_code_data(corpus_preferences["code"]))
-    print(language_data)
     return corpus_data
 
 def list_files(path: str, extensions: list[str]) -> list[str]:
@@ -23,22 +25,23 @@ def list_files(path: str, extensions: list[str]) -> list[str]:
 def generate_i18_data(languages: dict) -> dict:
     data = {}
     for language in languages.keys():
-        data[language] = generate_language_data(languages[language]["path"])
+        data[language] = load_dir_data(languages[language]["path"], ["txt"])
+        data[language]["weight"] = languages[language]["weight"]
     return data
 
-def generate_language_data(data_dir: str) -> dict:
+def load_dir_data(data_dir: str, extensions: list[str]) -> dict:
     data = {
         "letter_count": {},
         "bigram_count": {},
         "size": 0,
         "trigram_count": {},
     }
-    for _f in list_files(data_dir, ["txt"]):
+    for _f in list_files(data_dir, extensions):
         with open(_f, 'r') as file:
-            load_language_data(file, data)
+            load_file_data(file, data)
     return data
 
-def load_language_data(file, data) -> None:
+def load_file_data(file, data) -> None:
     for line in file.readlines():
         line = line.strip()
         for word in line.split():
@@ -59,4 +62,16 @@ def load_word_data(word, data):
 
 def generate_code_data(languages: dict) -> dict:
     data = {}
+    for language in languages.keys():
+        get_sources(languages[language]["git_urls"], languages[language]["path"])
+        data[language] = load_dir_data(languages[language]["path"], languages[language]["extensions"])
+        data[language]["weight"] = languages[language]["weight"]
+        print(data[language])
     return data
+
+def get_sources(urls: list[str], path: str) -> None:
+    for url in urls:
+        clone_path = path + "/" + url.replace("https://", "").replace("/","___")
+        print(url, clone_path)
+        if not os.path.exists(clone_path):
+            git.Repo.clone_from(url, clone_path)
