4b302a4e880c185b03c47ff190130bceb4752c59
Roming22 <roming22@gmail.com>
2022-12-31 01:49:29 -0500

    Add corpus: Add code projects

--- src/myapp/config_reader.py
+++ src/myapp/config_reader.py
@@ -13,14 +13,17 @@ def read(keyboard: str) -> dict:
         "keyboard": load_yaml(f"{config_dir}/keyboards/{keyboard}.yaml"),
         "preferences": load_yaml(f"{config_dir}/preferences.yaml"),
     }
-    add_path_to_corpus(config["preferences"]["corpus"], data_dir)
+    extend_corpus_data(config["preferences"]["corpus"], data_dir)
     return config
 
 def load_yaml(filepath: str) -> dict:
     with open(filepath, 'r') as file:
         return yaml.safe_load(file)
 
-def add_path_to_corpus(config: dict, data_dir:str) -> None:
+def extend_corpus_data(config: dict, data_dir:str) -> None:
     for _i in config.keys():
         for _j in config[_i].keys():
-            config[_i][_j]["path"] = f"{data_dir}/corpus/{_i}/{_j}"
+            path = f"{data_dir}/corpus/{_i}/{_j}"
+            if _i == "code":
+                config[_i][_j].update(load_yaml(path+"/config.yaml"))
+            config[_i][_j]["path"] = path
