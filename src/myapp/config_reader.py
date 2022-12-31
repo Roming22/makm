d8061a10f62d467c9255853ab5f24ae2fb8fe6f1
Roming22 <roming22@gmail.com>
2022-12-31 01:30:55 -0500

    Read preferences

--- src/myapp/config_reader.py
+++ src/myapp/config_reader.py
@@ -7,12 +7,20 @@ import yaml
 
 def read(keyboard: str) -> dict:
     """Read the configuration files"""
-    config_dir = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))+"/data/config")
+    data_dir = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))+"/data")
+    config_dir = f"{data_dir}/config"
     config = {
         "keyboard": load_yaml(f"{config_dir}/keyboards/{keyboard}.yaml"),
+        "preferences": load_yaml(f"{config_dir}/preferences.yaml"),
     }
+    add_path_to_corpus(config["preferences"]["corpus"], data_dir)
     return config
 
 def load_yaml(filepath: str) -> dict:
     with open(filepath, 'r') as file:
         return yaml.safe_load(file)
+
+def add_path_to_corpus(config: dict, data_dir:str) -> None:
+    for _i in config.keys():
+        for _j in config[_i].keys():
+            config[_i][_j]["path"] = f"{data_dir}/corpus/{_i}/{_j}"
